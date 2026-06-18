e186dcd0bc34ec62d03bd2dfc6267ed8e60fdace
Luis Acum <luisacum03@gmail.com>
2026-06-18 09:47:41 -0400

    Implementación de formulario
    Un formulario de registro planta, ademas de la instalación de Flutter Local notification

--- mcf/lib/crear_registro.dart
+++ mcf/lib/crear_registro.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class CrearRegistroPage extends StatelessWidget {
+class CrearRegistroPage extends StatefulWidget {
   const CrearRegistroPage({super.key});
+
+  @override
+  State<CrearRegistroPage> createState() => _CrearRegistroPageState();
+}
+
+class _CrearRegistroPageState extends State<CrearRegistroPage> {
+  String nombre = '';
+  String tipo = '';
+  String bluetooth = '';
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +18,62 @@ class CrearRegistroPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Crear Registro'),
       ),
-      body: const Center(
-        child: Text('Pantalla para crear registros'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text('Ingrese el nombre de la planta:'),
+
+            TextField(
+              onChanged: (texto) {
+                setState(() {
+                  nombre = texto;
+                });
+              },
+              decoration: const InputDecoration(
+                hintText: 'Ej: pedro',
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text('Nombre ingresado: $nombre'),
+
+            const Text('Ingrese el tipo de planta:'),
+
+            TextField(
+              onChanged: (texto) {
+                setState(() {
+                  tipo = texto;
+                });
+              },
+              decoration: const InputDecoration(
+                hintText: 'Ej: hierba',
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text('Tipo ingresado: $tipo'),
+
+            const Text('Ingrese la dirección Bluetooth:'),
+
+            TextField(
+              onChanged: (texto) {
+                setState(() {
+                  bluetooth = texto;
+                });
+              },
+              decoration: const InputDecoration(
+                hintText: 'Ej: AA:BB:CC:DD:EE:FF',
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Text('Bluetooth ingresado: $bluetooth'),
+          ],
+        ),
       ),
     );
   }
